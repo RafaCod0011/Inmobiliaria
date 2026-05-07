cb41e9c2c435258ac936c2d5112fb191ec133e61
Martin-Bece <martinbecerrasl7@gmail.com>
2026-05-07 19:42:59 -0300

    ApiClass

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.activity)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

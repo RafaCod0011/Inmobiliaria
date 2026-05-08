de35498bb3fc6a3d0e34541975eeca37e033366e
Martin-Bece <martinbecerrasl7@gmail.com>
2026-05-08 15:33:17 -0300

    Toast contraseña o usuario incorrectos

--- app/src/main/java/com/universidad/inmobiliaria/ui/login/LoginViewModel.java
+++ app/src/main/java/com/universidad/inmobiliaria/ui/login/LoginViewModel.java
@@ -65,6 +65,8 @@ public class LoginViewModel extends AndroidViewModel {
                         Log.d("Error", response.message()); // Mensaje de error que devuelve
                         Log.d("Error", response.code()+""); // Codigo del error
                         Log.d("Error", response.errorBody().toString()+""); // junto
+
+                        Toast.makeText(context, "Contraseña y/o Usuario Incorrecto", Toast.LENGTH_SHORT).show();
                     }
                 }
 

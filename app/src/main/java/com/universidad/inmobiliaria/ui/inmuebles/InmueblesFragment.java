e1469fd71b380e9c1a88336fed08fe3d097a6f72
Martin-Bece <martinbecerrasl7@gmail.com>
2026-05-14 19:39:23 -0300

    Item Inmueble y Recycler de la vista
    Trabajo en clase 14/05 con compañeros

--- app/src/main/java/com/universidad/inmobiliaria/ui/inmuebles/InmueblesFragment.java
+++ app/src/main/java/com/universidad/inmobiliaria/ui/inmuebles/InmueblesFragment.java
@@ -24,8 +24,8 @@ public class InmueblesFragment extends Fragment {
         binding = FragmentInmueblesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textReflow;
-        reflowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
         return root;
     }
 
